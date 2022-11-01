709a137f749968b362b01942958f880821aff1f9
Deane Barker <deane@deanebarker.net>
2022-11-01 03:36:24 +0000

    Fixed the standard JSON serialization

--- src/Transformers/SerializedJson.cs
+++ src/Transformers/SerializedJson.cs
@@ -1,4 +1,11 @@
-﻿using EPiServer.Validation;
+﻿using EPiServer.ContentApi.Core.Configuration;
+using EPiServer.ContentApi.Core.Serialization;
+using EPiServer.ContentApi.Core.Serialization.Internal;
+using EPiServer.ContentApi.Core.Serialization.Models;
+using EPiServer.DataAccess.Internal;
+using EPiServer.ServiceLocation;
+using EPiServer.Validation;
+using EPiServer.Web;
 using Parlot.Fluent;
 using System.Text.Json;
 
@@ -27,18 +34,31 @@ namespace DeaneBarker.Optimizely.Endpoints.Transformers
 
         public string Transform(string source, object model)
         {
-            return JsonSerializer.Serialize(((IEnumerable<ContentData>)model).Select(c => getSimplifiedObject((IContent)c)));
+            if (model == null)
+                throw new ArgumentNullException("Model cannot be null");
 
-            // This needs to get replaced with the RIGHT way to do this
-            // 2022-10-21: email into Aniel and Magnus
-            object getSimplifiedObject(IContent content)
+            var contentConvertingService = ServiceLocator.Current.GetInstance<ContentConvertingService>();
+            
+            var toSerialize = new List<object>();
+            foreach(var content in ((IEnumerable<ContentData>)model))
             {
-                return new
-                {
-                    name = content.Name,
-                    contentLink = content.ContentLink
-                };
+                var converterContext = new ConverterContext(
+                    contentReference: ((IContent)content).ContentLink,
+                    language: System.Globalization.CultureInfo.CurrentCulture,
+                    contentApiOptions: new ContentApiOptions("", false, false, ""),
+                    contextMode: ContextMode.Default,
+                    select: "",
+                    expand: "",
+                    excludePersonalizedContent: false
+                    );
+                toSerialize.Add(contentConvertingService.ConvertToContentApiModel((IContent)content, converterContext));
             }
+
+            var serializer = new Newtonsoft.Json.JsonSerializer();
+            var sw = new StringWriter();
+            serializer.Serialize(sw, toSerialize);
+            return sw.ToString();
+
         }
     }
 }
